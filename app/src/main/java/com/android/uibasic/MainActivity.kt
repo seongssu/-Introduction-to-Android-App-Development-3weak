12950bfe909fad52b8b36cd8029eada5dd8ac879
seongssu <loveforggong@gmail.com>
2023-08-01 11:56:26 +0900

    [PACK] : 🎁 실무에서 버튼디자인 뼈대

---
+++ app/src/main/java/com/android/uibasic/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.android.uibasic
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        setContentView(R.layout.text_views)
+    }
+
+    //    fun imageButton1onclicked(v:View){
+//        Toast.makeText(this,"알람이 울립니다.",Toast.LENGTH_SHORT).show()
+//    }
+    fun imageButton1onclicked(v: View) {
+        val btn = Intent(Intent.ACTION_VIEW, Uri.parse("https://m.naver.com"))
+        startActivity(btn)
+    }
+
+}
